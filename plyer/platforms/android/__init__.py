211f8e5b6acf133ddde9310e68e38322dd673206
Ryan Pessa <ryan@essential-elements.net>
2016-01-14 15:52:55 -0600

    fix activity namespace for p4a revamp

--- plyer/platforms/android/__init__.py
+++ plyer/platforms/android/__init__.py
@@ -1,12 +1,19 @@
 from os import environ
-from jnius import autoclass
+from jnius import autoclass, JavaException
 
 ANDROID_VERSION = autoclass('android.os.Build$VERSION')
 SDK_INT = ANDROID_VERSION.SDK_INT
 
 if 'PYTHON_SERVICE_ARGUMENT' in environ:
-    PythonService = autoclass('org.renpy.android.PythonService')
+    try:
+        PythonService = autoclass('org.kivy.android.PythonService')
+    except JavaException:
+        PythonService = autoclass('org.renpy.android.PythonService')
     activity = PythonService.mService
 else:
-    PythonActivity = autoclass('org.renpy.android.PythonActivity')
+    try:
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+    except JavaException:
+        PythonActivity = autoclass('org.renpy.android.PythonActivity')
     activity = PythonActivity.mActivity
+
